98c648f990994532982463f94ac6044cea57fb33
hugosenari <hugosenari@gmail.com>
2022-05-12 11:50:41 -0300

    chore: re index constants

--- flake.nix
+++ flake.nix
@@ -7,6 +7,14 @@
   inputs.flakeNimbleLib.type  = "github";
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
+  inputs."constants-master".dir   = "master";
+  inputs."constants-master".owner = "nim-nix-pkgs";
+  inputs."constants-master".ref   = "master";
+  inputs."constants-master".repo  = "constants";
+  inputs."constants-master".type  = "github";
+  inputs."constants-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."constants-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
   let 
     lib  = flakeNimbleLib.lib;
